329643938d58d15ade07d3b3f5db26578f5c3d61
huynhthiailinh <102190172@sv1.dut.udn.vn>
2022-06-26 04:13:46 +0700

    Handle load more messages when scroll on top

--- happy-care-mobile/src/components/screens/chat/ChatRoom/Message.js
+++ happy-care-mobile/src/components/screens/chat/ChatRoom/Message.js
@@ -1,59 +1,51 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'native-base';
 import { Image } from 'react-native';
 import { AsyncStoreHelper } from '../../../../api/helper';
 
 export const Message = (props) => {
   const { message, userId } = props;
-
-  const [isMyMessage, setIsMyMessage] = useState(false);
+  const [currentUserId, setCurrentUserId] = useState(null);
 
   useEffect(() => {
-    const checkIsMyMessage = async () => {
-      const id = await AsyncStoreHelper.getUserId();
-      setIsMyMessage(userId === id);
-    }
-    checkIsMyMessage();
+    const initCurrentUser = async () => {
+      setCurrentUserId(await AsyncStoreHelper.getUserId());
+    };
+    initCurrentUser();
   }, []);
 
   return (
-    <View style={{
-        alignSelf: isMyMessage ? 'flex-end' : 'flex-start',
+    <View
+      style={{
+        alignSelf: currentUserId === userId ? 'flex-end' : 'flex-start',
         padding: 10,
         margin: 10,
         borderRadius: 10,
         width: '70%',
-        flexDirection: 'row'
+        flexDirection: 'row',
       }}
-        bg= {isMyMessage ? 'blue.500' : 'muted.200'}
-      >
-        {
-          message.type === 'text' ? (
-          <Text
-            color={isMyMessage ? 'white' : 'black'}
-            borderRadius="8"
-          >
-            { message.content }
-          </Text>) :
-          (
-            message.type === 'image' ? (<Image 
-              source={{
-                uri: message.content
-              }}
-              style={{
-                resizeMode: 'contain',
-                flex: 1,
-                aspectRatio: 1
-              }}
-            />) :
-            (<Text
-              color={isMyMessage ? 'white' : 'black'}
-              borderRadius="8"
-            >
-              { message.content }
-            </Text>)
-          )
-        }
-      </View>
+      bg={currentUserId === userId ? 'blue.500' : 'muted.200'}
+    >
+      {message.type === 'text' ? (
+        <Text color={currentUserId === userId ? 'white' : 'black'} borderRadius="8">
+          {message.content}
+        </Text>
+      ) : message.type === 'image' ? (
+        <Image
+          source={{
+            uri: message.content,
+          }}
+          style={{
+            resizeMode: 'contain',
+            flex: 1,
+            aspectRatio: 1,
+          }}
+        />
+      ) : (
+        <Text color={currentUserId ? 'white' : 'black'} borderRadius="8">
+          {message.content}
+        </Text>
+      )}
+    </View>
   );
 };
